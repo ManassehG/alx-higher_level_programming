add6735ad9bf05b4db6f8e649d00a3e6c4664e0f
ManassehG <manasseh65mwas25@gmail.com>
2023-01-06 09:23:59 -0800

    Working on control structures in python

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,8 +2,8 @@
 import random
 number = random.randint(-10, 10)
 if number > 0:
-    print(number, 'is positive')
+    print("{:d} is positive".format(number))
+elif number < 0:
+    print("{:d} is negative".format(number))
 elif number == 0:
-    print(number, 'is zero')
-else:
-   print(number, 'is negative')
+    print("{:d} is zero".format(number))
