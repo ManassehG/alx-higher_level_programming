2dee18bddbd829038005402c6d24e34455633381
ManassehG <manasseh65mwas25@gmail.com>
2023-01-28 01:16:12 -0800

    Exceptions in python

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# 5-raise_exception.py
+
+def raise_exception():
+    """Raise a TypeError exception."""
+    raise TypeError
---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# 6-raise_exception_msg.py
+
+def raise_exception_msg(message=""):
+    """Raise a NameError exception with a message."""
+    raise NameError(message)
